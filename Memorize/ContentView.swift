1c111931d52e2e82219d8c97b2c47fe28888940b
yjh <1304877912@qq.com>
2025-04-20 21:54:32 +0800

    p2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    let emojis: [String] = ["👌","❤️","😳","😉"]
+//    let emojis: Array<String> = ["👌","❤️","😳","😉"]
+    let emojis = ["👌","❤️","😳","😉"]
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!2")
+        HStack {
+//            0..<4 = 0,1,2,3   0...4 = 0,1,2,3,4
+            ForEach(emojis.indices,id: \.self){ index in
+                    CardView(content: emojis[index])
+            }
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+struct CardView: View {
+    let content:String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+         let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                base.fill()
+                
+            }
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
